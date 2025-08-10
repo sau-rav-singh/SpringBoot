53bf97e192c3c59b3b62539ad6c80dbe60f63bba
Saurav Singh <singh.saurav@icloud.com>
2025-08-10 08:50:52 +0530

    Added DI

--- SpringOverview/src/main/java/config/AutoWiredConfig.java
+++ SpringOverview/src/main/java/config/AutoWiredConfig.java
@@ -5,22 +5,21 @@ import beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.parameters.P;
 
 @Configuration
 @ComponentScan(basePackages = "beans")
 public class AutoWiredConfig {
 
     @Bean
-    public Vehicle vehicle(){
-        Vehicle vehicle=new Vehicle();
+    public Vehicle vehicle() {
+        Vehicle vehicle = new Vehicle();
         vehicle.setName("Toyota");
         return vehicle;
     }
 
     @Bean
-    public Person person(Vehicle vehicle){
-        Person person=new Person(vehicle);
+    public Person person(Vehicle vehicle) {
+        Person person = new Person(vehicle);
         person.setName("AutoWired Lucy");
         return person;
     }
