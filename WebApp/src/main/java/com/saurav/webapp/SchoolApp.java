27344e46c612a2ce1883e3942912c699353a4e72
Saurav Singh <singh.saurav@icloud.com>
2025-08-20 19:31:20 +0530

    Added WebApp

---
+++ WebApp/src/main/java/com/saurav/webapp/SchoolApp.java
@@ -0,0 +1,13 @@
+package com.saurav.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApp.class, args);
+	}
+
+}
