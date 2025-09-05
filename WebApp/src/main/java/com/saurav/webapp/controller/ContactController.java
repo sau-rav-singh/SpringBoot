a827204f0f80a10cf70ecf648671f8efbf0e7fd9
Saurav Singh <singh.saurav@icloud.com>
2025-09-05 08:58:43 +0530

    Added Path Variable to Holiday

--- WebApp/src/main/java/com/saurav/webapp/controller/ContactController.java
+++ WebApp/src/main/java/com/saurav/webapp/controller/ContactController.java
@@ -26,23 +26,10 @@ public class ContactController {
         return "contact.html";
     }
 
-    /*@RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
-                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
-        log.info("Name : " + name);
-        log.info("Mobile Number : " + mobileNum);
-        log.info("Email Address : " + email);
-        log.info("Subject : " + subject);
-        log.info("Message : " + message);
-        return new ModelAndView("redirect:/contact");
-    }*/
-
-    @RequestMapping(value = "/saveMsg",method = POST)
-    public ModelAndView saveMessage(Contact contact){
+    @RequestMapping(value = "/saveMsg", method = POST)
+    public ModelAndView saveMessage(Contact contact) {
         contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
-
-
 
 }
