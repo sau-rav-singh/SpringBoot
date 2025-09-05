a76baffa661f6e8c2602af033f8e603b6345d354
Saurav Singh <singh.saurav@icloud.com>
2025-09-05 10:22:11 +0530

    Added Path Variable to Holiday

--- WebApp/src/main/java/com/saurav/webapp/controller/ContactController.java
+++ WebApp/src/main/java/com/saurav/webapp/controller/ContactController.java
@@ -2,11 +2,14 @@ package com.saurav.webapp.controller;
 
 import com.saurav.webapp.model.Contact;
 import com.saurav.webapp.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -22,14 +25,18 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to : {}", errors);
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
-
 }
